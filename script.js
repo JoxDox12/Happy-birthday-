5afd921c5472b1143474d13e97f914e8dd8fbb98
José <126896564+JoxDox12@users.noreply.github.com>
2024-04-19 18:01:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let show = () => {
+    let name = document.getElementById('name').value;
+    let age = document.getElementById('age').value;
+    document.getElementById('btn').addEventListener("click", show);
+}
